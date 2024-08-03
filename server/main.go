6cfc9550b51856fc517fc638541d70fa2ae29fd3
Shalitha Weerakoon Karunatilleke <shalitha@gmail.com>
2024-08-03 14:08:01 +1000

    Adding a goroutine example

--- server/main.go
+++ server/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"sync"
 
 	"grpc-post-body-test/chat"
 
@@ -62,22 +63,64 @@ func (s *ChatServiceServer) ServerStreamChat(message *chat.Message, stream chat.
 
 // Bidirectional streaming RPC implementation
 func (s *ChatServiceServer) BidirectionalStreamChat(stream chat.ChatService_BidirectionalStreamChatServer) error {
-	for {
-		message, err := stream.Recv()
-		if err == io.EOF {
-			return nil
-		}
-		if err != nil {
-			return err
-		}
+	// Goroutines are lightweight and scalable. They require minimal memory and startup time, allowing the server to handle many client streams concurrently without significant overhead.
+	var wg sync.WaitGroup
 
-		response := &chat.Message{
-			Body: "Received: " + message.Body,
+	messageCh := make(chan *chat.Message)
+
+	// Goroutine to receive messages from the client
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				if err == io.EOF {
+					close(messageCh)
+					return
+				}
+				log.Printf("Error receiving message: %v", err)
+				return
+			}
+			messageCh <- msg
 		}
-		if err := stream.Send(response); err != nil {
-			return err
+	}()
+
+	// Goroutine to send messages to the client
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for msg := range messageCh {
+			log.Printf("Received message from client: %s", msg.Body)
+			response := &chat.Message{Body: "Echo: " + msg.Body}
+			if err := stream.Send(response); err != nil {
+				log.Printf("Error sending message: %v", err)
+				return
+			}
 		}
-	}
+	}()
+
+	// Wait for both goroutines to complete
+	wg.Wait()
+	return nil
+
+	// TODO: Refactor. This was the original implementation wihout goroutines
+	// for {
+	// 	message, err := stream.Recv()
+	// 	if err == io.EOF {
+	// 		return nil
+	// 	}
+	// 	if err != nil {
+	// 		return err
+	// 	}
+
+	// 	response := &chat.Message{
+	// 		Body: "Received: " + message.Body,
+	// 	}
+	// 	if err := stream.Send(response); err != nil {
+	// 		return err
+	// 	}
+	// }
 }
 
 func joinMessages(messages []string) string {
